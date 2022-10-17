13098b0364ddc428b2397492b6c9b2b7b974f316
Alexander Shar <alahan05@gmail.com>
2022-10-17 09:16:24 +0300

    1st tsk ended

---
+++ task1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] RandomArrGen()
+{
+    int length = new Random().Next(1, 15);
+    int[] arr = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+
+}
+void ArrShow( int[] array )
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
+int CheckingEven( int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if( array[i] % 2 == 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int[] arr = RandomArrGen();
+ArrShow(arr);
+System.Console.WriteLine(CheckingEven(arr));
