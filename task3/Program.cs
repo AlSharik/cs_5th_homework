186faab617f0164e4d4800bf3927412aff7f23fd
Alexander Shar <alahan05@gmail.com>
2022-10-17 09:42:01 +0300

    3rd task ended

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+
+// В условиях сказанно о вещественных числах поэтому все через double
+
+double[] RandomArrGen()
+{
+    double max = - 10000;
+    double min = 100000;
+    int length = new Random().Next(1, 15);
+    double[] arr = new double[length];
+    for(int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().NextDouble()*2000-1000;
+        System.Console.WriteLine(arr[i]);
+        if(max < arr[i]) max = arr[i];
+        if(min > arr[i]) min = arr[i];
+    }
+    System.Console.WriteLine($"Разница равна {max - min}");
+    return arr;
+
+}
+double[] arr = RandomArrGen();
